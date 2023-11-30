d673f2062586652fecdee497097198fb32283a8c
TranTrong Nguyen <nguyen872001@gmail.com>
2023-11-30 11:58:21 +0700

    update token

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -28,31 +28,24 @@ namespace BE_Shop.Controllers
                         if (User.Claims.Any())
                         {
                             var user = db._User.Find(Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value ?? throw new HttpException(string.Empty, 401)));
-                            if (user.Status == 0 && user.TokenKey == User.Claims.FirstOrDefault(c => c.Type == "Key")?.Value)
+                            a.Query_DataInput(input);
+                            user.TokenKey = Converter.RamdomByte(32);
+                            db.SaveChanges();
+                            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                            SecurityToken token = tokenHandler.CreateToken(new SecurityTokenDescriptor
                             {
-                                a.Query_DataInput(input);
-                                user.TokenKey = Converter.RamdomByte(32);
-                                db.SaveChanges();
-                                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-                                SecurityToken token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+                                Subject = new ClaimsIdentity(new Claim[]
                                 {
-                                    Subject = new ClaimsIdentity(new Claim[]
-                                    {
-                                    new Claim(ClaimTypes.Name, user.Id.ToString()),
-                                    new Claim(ClaimTypes.Role, user.Role),
-                                    new Claim("Key", user.TokenKey),
-                                    }),
-                                    Expires = DateTime.Now.AddMinutes(5),
-                                    SigningCredentials = new SigningCredentials(
-                                        new SymmetricSecurityKey(Encoding.ASCII.GetBytes(UserController.key)),
-                                        SecurityAlgorithms.HmacSha256Signature)
-                                });
-                                ResetToken = "Bearer " + tokenHandler.WriteToken(token);
-                            }
-                            else
-                            {
-                                throw new HttpException(string.Empty, 403);
-                            }
+                                new Claim(ClaimTypes.Name, user.Id.ToString()),
+                                new Claim(ClaimTypes.Role, user.Role),
+                                new Claim("Key", user.TokenKey),
+                                }),
+                                Expires = DateTime.Now.AddMinutes(5),
+                                SigningCredentials = new SigningCredentials(
+                                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(UserController.key)),
+                                    SecurityAlgorithms.HmacSha256Signature)
+                            });
+                            ResetToken = "Bearer " + tokenHandler.WriteToken(token);
                         }
                     }
                 });
