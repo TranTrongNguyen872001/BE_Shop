92ec9ed0c046a39895b6613e7acf5de99359b2e9
VuongCaoDuongTrung <117805268+VuongCaoDuongTrung@users.noreply.github.com>
2023-12-01 17:55:36 +0700

    Update NotificationHub.cs

--- Hubs/NotificationHub.cs
+++ Hubs/NotificationHub.cs
@@ -1,44 +1,152 @@
-﻿using Microsoft.AspNetCore.SignalR;
+using BE_Shop.Controllers;
 using BE_Shop.Data;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Principal;
+using BE_Shop.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
 using System.Text.RegularExpressions;
 
 namespace BE_Shop.Hubs
 {
-	public class NotificationHub : Hub
-	{
+    [Authorize]
+    public class MessageHub : Hub
+    {
+        public readonly static List<UserViewModel> _Connections = new List<UserViewModel>();
+        private readonly static Dictionary<string, string> _ConnectionsMap = new Dictionary<string, string>();
+
+        private readonly DatabaseConnection _context;
+        private readonly IMapper _mapper;
+
+        public MessageHub(DatabaseConnection context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        private string IdentityName
+        {
+            get { return Context.User.Identity.Name;}
+        }
+        public async Task SendPrivate(string receiverName, string message)
+        {
+            if (_ConnectionsMap.TryGetValue(receiverName, out string userId))
+            {
+                // Who is the sender;
+                var sender = _Connections.Where(u => u.UserName == IdentityName).First();
+
+                if (!string.IsNullOrEmpty(message.Trim()))
+                {
+                    // Build the message
+                    var messageViewModel = new MessageViewModel()
+                    {
+                        Content = Regex.Replace(message, @"<.*?>", string.Empty),
+                        FromUserName = sender.UserName,
+                        FromFullName = sender.FullName,
+                        Avatar = sender.Avatar,
+                        Room = "",
+                        CreatedDate = DateTime.Now
+                    };
+
+                    // Send the message
+                    await Clients.Client(userId).SendAsync("newMessage", messageViewModel);
+                    await Clients.Caller.SendAsync("newMessage", messageViewModel);
+                }
+            }
+        }
+
+        public async Task Join(string roomName)
+        {
+            try
+            {
+                var user = _Connections.Where(u => u.UserName == IdentityName).FirstOrDefault();
+                if (user != null && user.CurrentRoom != roomName)
+                {
+                    // Remove user from others list
+                    if (!string.IsNullOrEmpty(user.CurrentRoom))
+                        await Clients.OthersInGroup(user.CurrentRoom).SendAsync("removeUser", user);
+
+                    // Join to new chat room
+                    await Leave(user.CurrentRoom);
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+                    user.CurrentRoom = roomName;
+
+                    // Tell others to update their list of users
+                    await Clients.OthersInGroup(roomName).SendAsync("addUser", user);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Clients.Caller.SendAsync("onError", "Vô phòng lỗi!" + ex.Message);
+            }
+        }
+
+        public async Task Leave(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
+
+        public IEnumerable<UserViewModel> GetUsers(string roomName)
+        {
+            return _Connections.Where(u => u.CurrentRoom == roomName).ToList();
+        }
+
         public override Task OnConnectedAsync()
         {
-            Clients.Caller.SendAsync("OnConnected");
+            try
+            {
+                var user = _context._User.Where(u => u.UserName == IdentityName).FirstOrDefault();
+                var userViewModel = _mapper.Map<User, UserViewModel>(user);
+
+                if (!_Connections.Any(u => u.UserName == IdentityName))
+                {
+                    _Connections.Add(userViewModel);
+                    _ConnectionsMap.Add(IdentityName, Context.ConnectionId);
+                }
+
+                Clients.Caller.SendAsync("getProfileInfo", userViewModel);
+            }
+            catch (Exception ex)
+            {
+                Clients.Caller.SendAsync("onError", "OnConnected:" + ex.Message);
+            }
             return base.OnConnectedAsync();
         }
-        public override Task OnDisconnectedAsync(Exception? exception)
+
+        public override Task OnDisconnectedAsync(Exception exception)
         {
-            Clients.Caller.SendAsync("OnDisconnected");
+            try
+            {
+                var user = _Connections.Where(u => u.UserName == IdentityName).First();
+                _Connections.Remove(user);
+
+                // Tell other users to remove you from their list
+                Clients.OthersInGroup(user.CurrentRoom).SendAsync("removeUser", user);
+
+                // Remove mapping
+                _ConnectionsMap.Remove(user.UserName);
+            }
+            catch (Exception ex)
+            {
+                Clients.Caller.SendAsync("onError", "OnDisconnected: " + ex.Message);
+            }
+
             return base.OnDisconnectedAsync(exception);
         }
-        public async Task Join(string group)
-        {
-            await Groups.AddToGroupAsync(Context.ConnectionId, group);
-            await Clients.OthersInGroup(group).SendAsync("Joined", Context.ConnectionId);
-        }
-        public async Task Leave(string group)
-        {
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
-            await Clients.Group(group).SendAsync("Leaved", Context.ConnectionId);
-        }
         public async Task SendToAll(object message)
-		{
-			await Clients.All.SendAsync("Received", new {ClientsId = Context.ConnectionId, Message = message});
-		}
-		public async Task SendToClient(object message, string ConnectionId)
-		{
+        {
+            await Clients.All.SendAsync("Received", new { ClientsId = Context.ConnectionId, Message = message });
+        }
+        public async Task SendToClient(object message, string ConnectionId)
+        {
             await Clients.Client(ConnectionId).SendAsync("Received", new { ClientsId = Context.ConnectionId, Message = message });
         }
-		public async Task SendToGroup(object message, string group)
-		{
+        public async Task SendToGroup(object message, string group)
+        {
             await Clients.OthersInGroup(group).SendAsync("Received", new { ClientsId = Context.ConnectionId, Message = message });
         }
-	}
+    }
+
 }
